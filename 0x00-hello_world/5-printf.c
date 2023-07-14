e3054df6e732af2a78cbe0f02eaa76399b32c997
appleson1 <emmaandsax@gmail.com>
2023-07-14 10:09:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program:
+ *
+ * Description: program that prints exactly with proper grammar:
+ * "but the outcome is a piece of art"
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
