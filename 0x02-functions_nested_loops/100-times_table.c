07e2b4a745a5595693c79a83d7fecf1ed09a0628
appleson1 <emmaandsax@gmail.com>
2023-07-19 15:19:59 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,53 +7,52 @@ int _putchar(char c);
 
 void print_times_table(int n)
 {
-    int i = 0;
-    int j, res;
+	int i = 0;
+	int j, res;
 
-    if (n < 0 || n > 15)
-    {
-        return;
-    }
-    else
-    {
-        while (i <= n)
-        {
-            j = 0;
-            while (j <= n)
-            {
-                if (j != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
-                res = i * j;
-                if (j == 0)
-                {
-                    _putchar(res + '0');
-                }
-                else if (res < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(res + '0');
-                }
-                else if (res < 100)
-                {
-                    _putchar(' ');
-                    _putchar(res / 10 + '0');
-                    _putchar(res % 10 + '0');
-                }
-                else
-                {
-                    _putchar(res / 100 + '0');
-                    _putchar((res / 10) % 10 + '0');
-                    _putchar(res % 10 + '0');
-                }
-                j++;
-            }
-            _putchar('\n');
-            i++;
-        }
-    }
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	else
+	{
+		while (i <= n)
+		{
+			j = 0;
+			while (j <= n)
+			{
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				res = i * j;
+				if (j == 0)
+				{
+					_putchar(res + '0');
+				}
+				else if (res < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
+				}
+				else if (res < 100)
+				{
+					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				else
+				{
+					_putchar(res / 100 + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
 }
-
